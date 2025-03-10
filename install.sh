585a71878dd2bb9af1f1f75c10bfe52a68daa4be
Gregor Rosenauer <gregor.rosenauer@gmail.com>
2025-03-10 14:01:01 +0100

    add output for success/error in updating Tracker.

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 HAIKU_OUT_DIR=$HOME/Develop/haiku/generated/objects/haiku/x86_64/release
 
 cp $HAIKU_OUT_DIR/kits/tracker/libtracker.so ./generated/lib/ && \
-cp $HAIKU_OUT_DIR/apps/tracker/Tracker ./generated/Tracker
+cp $HAIKU_OUT_DIR/apps/tracker/Tracker ./generated/Tracker && \
+echo "TrackerSEN updated and ready to roll." ||
+echo "Error updating TrackerSEN."
